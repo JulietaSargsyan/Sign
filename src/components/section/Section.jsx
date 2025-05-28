bb98948d5cf005e1e5894279d60bff376467d3de
Julieta Sargsyan <julietasargsian@gmail.com>
2025-05-28 19:38:01 +0400

    Animate with motion frame

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import styles from './Section.module.css'
-import Header from '../header/Header';
+import { motion } from "framer-motion";
 
 function Section({ children, sectionName, theme = '', id = '' }) {
   return ( 
-    <section id={id} className={`${styles.section} ${styles[sectionName]} ${styles[theme]}`}>
+    <motion.div
+      initial={{ opacity: 0, y: 40 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.7 }}
+      viewport={{ amount: 0.2 }} 
+      id={id} 
+      className={`${styles.section} ${styles[sectionName]} ${styles[theme]}`}
+    >
       {children}
-    </section>
+    </motion.div>
   )
 }
 
--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -2,8 +2,10 @@
 
 import {useState} from 'react';
 import Link from 'next/link';
-import styles from './SideBar.module.css';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
+
+import styles from './SideBar.module.css';
 import NavBar from '../navigation/NavBar';
 
 import cross from '../../assets/sideBarCross.png'
@@ -11,11 +13,16 @@ import cross from '../../assets/sideBarCross.png'
 function SideBar({ open, handleClick }) {
   return (
     <div className={styles.sideBar}>
-      <div className={`${styles.burgerMenu} ${open ? styles.open : ''}`} onClick={() => handleClick()}>
+      <motion.button 
+        whileHover={{ scale: 0.9 }} 
+        whileTap={{ scale: 1.1 }}
+        className={`${styles.burgerMenu} ${open ? styles.open : ''}`} 
+        onClick={() => handleClick()}
+      >
         <div></div>
         <div></div>
         <div></div>
-      </div>
+      </motion.button>
       <Image src={cross} alt='sign cross' width={14} height={14}/>
       <Link onClick={(e) => handleClick(e.target)} className={styles.sideBar__navListItem} href="https://julietasargsyan.github.io/Sign/#heroSection">hi there</Link>
       <Link onClick={handleClick} className={styles.sideBar__navListItem} href="https://julietasargsyan.github.io/Sign/#whoWeAreSection">who we are</Link>
