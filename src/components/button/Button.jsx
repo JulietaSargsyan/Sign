d3cab2730fd3f626ad6cc04fadf525ea703fb15d
Julieta Sargsyan <julietasargsian@gmail.com>
2025-05-29 11:58:20 +0400

    Add animation on button hover

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,18 +1,66 @@
+'use client'
+
+import { useState } from "react";
 import Link from "next/link";
+
 import styles from "./Button.module.css";
 
 function Button({ theme, text, href }) {
+  const [isMouseEntered, setIsMouseEntered] = useState(false)
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
   const isDownload = href.match(/\.(pdf|jpg|png|zip|txt|csv|json)$/i);
+
+  const handleMouseEnter = (e) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    
+    setCoords({ x, y });
+    setIsMouseEntered(prevState => !prevState);
+  };
+
+  function handleMouseLeave(e) {
+    const rect = e.currentTarget.getBoundingClientRect();
+    
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    
+    setCoords(prevCoords => ({ ...prevCoords, x: x, y: y }));
+    setIsMouseEntered(prevState => !prevState);
+  };
 
   return (
     <div>
       {isDownload ? (
         <a href={href} download className={`${styles.buttonContainer} ${styles[theme]}`}>
-          <button className={styles.button}>{text}</button>
+          <button 
+            className={styles.button}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <p style={{ color: isMouseEntered ? 'white' : ''}}>{text}</p>
+            <span className={`${styles.wave} ${isMouseEntered ? styles.finSize : ''}`} 
+              style={{
+              top: coords.y + 'px',
+              left:coords.x + 'px',
+              }}></span>
+          </button>
         </a>
       ) : (
         <Link href={href} className={`${styles.buttonContainer} ${styles[theme]}`}>
-          <button className={styles.button}>{text}</button>
+          <button 
+            className={styles.button}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <p style={{ color: isMouseEntered ? 'white' : ''}}>{text}</p>
+            <span className={`${styles.wave} ${isMouseEntered ? styles.finSize : ''}`} 
+              style={{
+              top: coords.y + 'px',
+              left:coords.x + 'px',
+              }}></span>
+          </button>
         </Link>
       )}
     </div>
