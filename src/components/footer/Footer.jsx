23483c350dd2d905feebcbe25f687d6702cf72e0
Julieta Sargsyan <julietasargsian@gmail.com>
2025-06-18 10:51:16 +0400

    Update footer info

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,10 +3,10 @@ import styles from './Footer.module.css'
 
 const Footer = () => (
     <footer className={styles.footer}>
-      <p>&copy; 2024 All rights reserved. Sign Digital</p>
+      <p>&copy; 2025 All rights reserved. Sign Digital</p>
       <Link className={styles.social} href="https://www.linkedin.com/in/sign-digital-design-studio-109a56339/" target="_blank" rel="noopener noreferrer">LinkedIn</Link>
       <Link className={styles.social} href="https://www.facebook.com/Sign-2213681662203036" target="_blank" rel="noopener noreferrer">Facebook</Link>
-      <Link className={styles.social} href="https://www.instagram.com/sign_digital.studio/" target="_blank" rel="noopener noreferrer">Instagram</Link>
+      <Link className={styles.social} href="https://www.instagram.com/sign_digital_design_studio/" target="_blank" rel="noopener noreferrer">Instagram</Link>
     </footer>
   );
   
