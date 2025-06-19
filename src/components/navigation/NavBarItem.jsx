a7fea6f72e13b7dc50af77e1e3f5a125b33e6437
Julieta Sargsyan <julietasargsian@gmail.com>
2025-06-19 09:59:51 +0400

    Fix: highlight active nav item

--- src/components/navigation/NavBarItem.jsx
+++ src/components/navigation/NavBarItem.jsx
@@ -1,14 +1,19 @@
+'use client';
+
 import Image from "next/image"
 import Link from "next/link"
+import { usePathname  } from 'next/navigation';
 
 import styles from './NavBar.module.css'
 import signStarImg from '../../assets/Sign_Star_Purple.svg';
 
 function NavBarItem({ href, text, handleClick }) {
+  const pathname = usePathname();
+
   return (
     <li className={styles.navigation__item}>
         <Link onClick={() => handleClick()} href={href}>{text}</Link>
-        <Image src={signStarImg} alt="sign digital star" width={61} height={85} className="star-shape" />
+        <Image className={`${pathname === href ? styles.active : ''} star-shape`} src={signStarImg} alt="sign digital star" width={61} height={85} />
     </li>
   )
 }
