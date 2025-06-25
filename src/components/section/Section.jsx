46c69b8879daf87e0e0d0bb21bc352e38c1413c6
Julieta Sargsyan <julietasargsian@gmail.com>
2025-06-25 16:55:12 +0400

    Cleanup

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -4,10 +4,6 @@ import styles from './Section.module.css'
 function Section({ children, sectionName, theme = '', id = '' }) {
   return ( 
     <section
-      initial={{ opacity: 0, y: 40 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.7 }}
-      viewport={{ amount: 0.2 }} 
       id={id} 
       className={`section ${styles.section} ${styles[sectionName]} ${styles[theme]}`}
     >
