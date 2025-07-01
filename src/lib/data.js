48eb0ba2c8b042d4dc77e0d28ade76c355e14dfc
Julieta Sargsyan <julietasargsian@gmail.com>
2025-07-01 18:20:56 +0400

    Add example data

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,14 +1,104 @@
-const portfolioData = [
+import consultingGroupLogo from '@/assets/consultingGroup/consultingGroupLogo.png';
+import consultingGroupBanner from '@/assets/consultingGroup/consultingGroupBanner.png';
+import consultingGroup1 from '@/assets/consultingGroup/consultingGroup1.png';
+import consultingGroup2 from '@/assets/consultingGroup/consultingGroup2.png';
+import consultingGroup3 from '@/assets/consultingGroup/consultingGroup3.png';
+import consultingGroup4 from '@/assets/consultingGroup/consultingGroup4.png';
+import consultingGroup5 from '@/assets/consultingGroup/consultingGroup5.png';
+import consultingGroup6 from '@/assets/consultingGroup/consultingGroup6.png';
+import consultingGroup7 from '@/assets/consultingGroup/consultingGroup7.png';
+import consultingGroup8 from '@/assets/consultingGroup/consultingGroup8.png';
+import consultingGroup9 from '@/assets/consultingGroup/consultingGroup9.png';
+
+export const portfolioData = [
     {
-      name: 'DataDummy',
-      amount: 15795,
-      status: 'pending',
-      date: '2022-12-06',
+      id: 1,
+      slug: 'consultingGroup',
+      name: 'Consulting group',
+      client: 'Consulting Group LLC',
+      category: 'Branding, Logo Design',
+      date: 'May 2020',
+      location: 'Yerevan, Armenia',
+      title: 'Crafting a Trustworthy Brand Identity for Consulting Group',
+      primaryImage: consultingGroupLogo,
+      bannerImage: consultingGroupBanner,
+      images: [consultingGroup1, consultingGroup2, consultingGroup3, consultingGroup4, consultingGroup5, consultingGroup6, consultingGroup7, consultingGroup8, consultingGroup9],
+      content: 'Sign Studio had the privilege of collaborating with Consulting Group, a prominent consultancy firm specializing in providing expert advice on business strategy and decision-making. Our mission was to create a brand identity that would reflect Consulting Group\'s core values of trustworthiness, professionalism, and reliability while making a lasting visual impression.',
+      challenge: 'At vero eos et accusamus etiusto odio dignissimos ducimus blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi occaecati cupiditate similique sunin culpa qui officia deserunt mollitia animi est laborum et dolorum fuga. Et harum quidem rerum facilis esty expedita distinctio.'
     },
     {
-      name: 'DataDummy',
-      amount: 20348,
-      status: 'pending',
-      date: '2022-11-14',
-    }
+      id: 2,
+      slug: 'consultingGroup',
+      name: 'Consulting group',
+      client: 'Consulting Group LLC',
+      category: 'Branding, Logo Design',
+      date: 'May 2020',
+      location: 'Yerevan, Armenia',
+      title: 'Crafting a Trustworthy Brand Identity for Consulting Group',
+      primaryImage: consultingGroupLogo,
+      bannerImage: consultingGroupBanner,
+      images: [consultingGroup1, consultingGroup2, consultingGroup3, consultingGroup4, consultingGroup5, consultingGroup6, consultingGroup7, consultingGroup8, consultingGroup9],
+      content: 'Sign Studio had the privilege of collaborating with Consulting Group, a prominent consultancy firm specializing in providing expert advice on business strategy and decision-making. Our mission was to create a brand identity that would reflect Consulting Group\'s core values of trustworthiness, professionalism, and reliability while making a lasting visual impression.',
+      challenge: 'At vero eos et accusamus etiusto odio dignissimos ducimus blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi occaecati cupiditate similique sunin culpa qui officia deserunt mollitia animi est laborum et dolorum fuga. Et harum quidem rerum facilis esty expedita distinctio.'
+    },
+    {
+      id: 3,
+      slug: 'consultingGroup',
+      name: 'Consulting group',
+      client: 'Consulting Group LLC',
+      category: 'Branding, Logo Design',
+      date: 'May 2020',
+      location: 'Yerevan, Armenia',
+      title: 'Crafting a Trustworthy Brand Identity for Consulting Group',
+      primaryImage: consultingGroupLogo,
+      bannerImage: consultingGroupBanner,
+      images: [consultingGroup1, consultingGroup2, consultingGroup3, consultingGroup4, consultingGroup5, consultingGroup6, consultingGroup7, consultingGroup8, consultingGroup9],
+      content: 'Sign Studio had the privilege of collaborating with Consulting Group, a prominent consultancy firm specializing in providing expert advice on business strategy and decision-making. Our mission was to create a brand identity that would reflect Consulting Group\'s core values of trustworthiness, professionalism, and reliability while making a lasting visual impression.',
+      challenge: 'At vero eos et accusamus etiusto odio dignissimos ducimus blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi occaecati cupiditate similique sunin culpa qui officia deserunt mollitia animi est laborum et dolorum fuga. Et harum quidem rerum facilis esty expedita distinctio.'
+    },
+    {
+      id: 4,
+      slug: 'consultingGroup',
+      name: 'Consulting group',
+      client: 'Consulting Group LLC',
+      category: 'Branding, Logo Design',
+      date: 'May 2020',
+      location: 'Yerevan, Armenia',
+      title: 'Crafting a Trustworthy Brand Identity for Consulting Group',
+      primaryImage: consultingGroupLogo,
+      bannerImage: consultingGroupBanner,
+      images: [consultingGroup1, consultingGroup2, consultingGroup3, consultingGroup4, consultingGroup5, consultingGroup6, consultingGroup7, consultingGroup8, consultingGroup9],
+      content: 'Sign Studio had the privilege of collaborating with Consulting Group, a prominent consultancy firm specializing in providing expert advice on business strategy and decision-making. Our mission was to create a brand identity that would reflect Consulting Group\'s core values of trustworthiness, professionalism, and reliability while making a lasting visual impression.',
+      challenge: 'At vero eos et accusamus etiusto odio dignissimos ducimus blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi occaecati cupiditate similique sunin culpa qui officia deserunt mollitia animi est laborum et dolorum fuga. Et harum quidem rerum facilis esty expedita distinctio.'
+    },
+    {
+      id: 5,
+      slug: 'consultingGroup',
+      name: 'Consulting group',
+      client: 'Consulting Group LLC',
+      category: 'Branding, Logo Design',
+      date: 'May 2020',
+      location: 'Yerevan, Armenia',
+      title: 'Crafting a Trustworthy Brand Identity for Consulting Group',
+      primaryImage: consultingGroupLogo,
+      bannerImage: consultingGroupBanner,
+      images: [consultingGroup1, consultingGroup2, consultingGroup3, consultingGroup4, consultingGroup5, consultingGroup6, consultingGroup7, consultingGroup8, consultingGroup9],
+      content: 'Sign Studio had the privilege of collaborating with Consulting Group, a prominent consultancy firm specializing in providing expert advice on business strategy and decision-making. Our mission was to create a brand identity that would reflect Consulting Group\'s core values of trustworthiness, professionalism, and reliability while making a lasting visual impression.',
+      challenge: 'At vero eos et accusamus etiusto odio dignissimos ducimus blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi occaecati cupiditate similique sunin culpa qui officia deserunt mollitia animi est laborum et dolorum fuga. Et harum quidem rerum facilis esty expedita distinctio.'
+    },
+    {
+      id: 6,
+      slug: 'consultingGroup',
+      name: 'Consulting group',
+      client: 'Consulting Group LLC',
+      category: 'Branding, Logo Design',
+      date: 'May 2020',
+      location: 'Yerevan, Armenia',
+      title: 'Crafting a Trustworthy Brand Identity for Consulting Group',
+      primaryImage: consultingGroupLogo,
+      bannerImage: consultingGroupBanner,
+      images: [consultingGroup1, consultingGroup2, consultingGroup3, consultingGroup4, consultingGroup5, consultingGroup6, consultingGroup7, consultingGroup8, consultingGroup9],
+      content: 'Sign Studio had the privilege of collaborating with Consulting Group, a prominent consultancy firm specializing in providing expert advice on business strategy and decision-making. Our mission was to create a brand identity that would reflect Consulting Group\'s core values of trustworthiness, professionalism, and reliability while making a lasting visual impression.',
+      challenge: 'At vero eos et accusamus etiusto odio dignissimos ducimus blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi occaecati cupiditate similique sunin culpa qui officia deserunt mollitia animi est laborum et dolorum fuga. Et harum quidem rerum facilis esty expedita distinctio.'
+    },
   ];
