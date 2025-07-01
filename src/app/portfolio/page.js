15be144b0d7a793ac253337b26c552ea5f36b4ee
Julieta Sargsyan <julietasargsian@gmail.com>
2025-07-01 20:23:43 +0400

    Add portfolio page

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,9 +1,40 @@
-import React from 'react'
+'use client'
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { portfolioData } from '@/lib/data.js';
+
+import './style.css'
 
 const PortfolioPage = () => {
   return (
-    <div>Portfolio Page</div>
-  )
+    <div className='portfolioPage-container'>
+      <h1 className='title'>Our Clients</h1>
+      <div className='portfolio-items'>
+        {portfolioData.map((item, index) => (
+          <motion.div 
+            initial={{x: index % 2 === 0 ? 2000 : -2000}} 
+            animate={{ x: 0}} 
+            transition={{
+              type: 'spring',
+              stiffness: 120,
+              damping: 10,
+              duration: 1
+            }} 
+            key={item.id} 
+          >
+            <Link href={`/portfolio/${item.slug}`} className='portfolio-item'>
+              <div className='portfolio-item__img'>
+                <Image src={item.primaryImage} alt={item.name} fill/>
+              </div>
+              <span className='portfolio-item__name'>{item.name}</span>
+            </Link>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default PortfolioPage
--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -11,7 +11,7 @@ function NavBar({ open, handleClick }) {
     <nav className={`${styles.navigation} ${open ? styles.open : ''}`}>
       <ul>
         <NavBarItem handleClick={handleClick} href='/about' text='about us'/>
-        <NavBarItem handleClick={handleClick} href='' text='portfolio'/>
+        <NavBarItem handleClick={handleClick} href='/portfolio' text='portfolio'/>
         <NavBarItem handleClick={handleClick} href='/services' text='services'/>
         <NavBarItem handleClick={handleClick} href='/contact' text='contact us'/>
       </ul>
